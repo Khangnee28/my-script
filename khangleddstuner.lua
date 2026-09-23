c236de5fe7fefef5fb9fa7b311d5d3a49f96a268
Khangnee28 <letrongkhang098@gmail.com>
2026-09-23 09:46:01 +0700

    Update khangleddstuner.lua

--- khangleddstuner.lua
+++ khangleddstuner.lua
@@ -2768,7 +2768,7 @@ local function of_findNearestUntriedSeat(pos, radius, tried)
            and p.Occupant == nil
            and not tried[p] then
             local d = (p.Position - pos).Magnitude
-if d >= 50 and d < bestD then best, bestD = p, d end        end
+if d >= 40 and d < bestD then best, bestD = p, d end        end
     end
     return best
 end
