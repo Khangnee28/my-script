2f15f335d6f088ad4558c0b08fd606ca0b410edb
Khangnee28 <letrongkhang098@gmail.com>
2026-09-25 17:00:03 +0700

    Update ridegodds.lua

--- ridegodds.lua
+++ ridegodds.lua
@@ -386,7 +386,7 @@ if os.clock() - lastCheck > 0.15 then
     lastCheck = os.clock()
             end
         end              
-        end
+        end)
 end)                 
 
     if bv and bv.Parent then bv:Destroy() end
