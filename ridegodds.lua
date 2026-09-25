3f35da508d1ea7c91aab11e7809f5ff39a086942
Khangnee28 <letrongkhang098@gmail.com>
2026-09-25 11:16:01 +0700

    Update ridegodds.lua

--- ridegodds.lua
+++ ridegodds.lua
@@ -403,10 +403,10 @@ local function runTrip()
     end
     myCar = findCarByName(selectedCar) or findMyCar()
 setNoclip(true)
-setState("chờ đơn")
+setState("sẵn sàng")
 return true
 
-    
+    setState("chờ đơn")
     orderToken = nil
     pickupPos = nil
     local deadline = os.clock() + ORDER_TIMEOUT
